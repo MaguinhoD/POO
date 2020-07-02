602aa43e95a7228603c31a25b5639ce0a4641150
Vitor <Vitor@192.168.0.79>
2020-07-02 07:52:35 -0300

    Lista 6 - Estabelecimento de Petsitting

---
+++ Petsitter/src/cadastros/Cliente.java
@@ -0,0 +1,75 @@
+package cadastros;
+
+import cadastros.Endereco;
+import cadastros.Telefone;
+import cadastros.Animal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente {
+	
+	private String nome;
+	private String cpf;
+	public Telefone telefone;
+	public Endereco endereco;
+	public List<Animal> animais = new ArrayList<Animal>();
+	
+	public Cliente(String nome, String cpf, Telefone telefone, Endereco endereco, List<Animal> animais) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.telefone = telefone;
+		this.endereco = endereco;
+		this.animais = animais;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public void imprimir() {
+		System.out.println("Nome: "+nome);
+		System.out.println("CPF: "+cpf);
+		endereco.imprimirEnd();
+		telefone.imprimirTel();
+		for (Animal pet : animais) {
+			System.out.println("Animal:");
+			pet.imprimirPet();
+		}
+	}
+		
+	public List<String> imprimiranimal() {
+		List<String> gen = new ArrayList<String>();
+		for (Animal pet : animais) {
+			gen.add(pet.getGenero());
+		}
+		return gen;
+	}
+
+
+	public List<Animal> getAnimais() {
+		return animais;
+	}
+
+
+	public void setAnimais(List<Animal> animais) {
+		this.animais = animais;
+	}
+		
+		
+	
+
+}
